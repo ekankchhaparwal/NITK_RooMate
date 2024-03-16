b3a4f37e996b185e3b5695ba4cde3ed68990763a
Ekank Chhaparwal <ekank1410@gmail.com>
2024-03-16 10:20:56 +0530

    Add room swap request

--- routes/register.js
+++ routes/register.js
@@ -77,4 +77,71 @@ router.post('/signUp', (req, res) => {
 });
 
 
+router.post('/addPersonToAvailableRooms', (req, res) => {
+  const {addToAvailableRooms } = req.body;
+  const email = req.session.user['email'];
+  console.log(email);
+    connection.query(
+      'SELECT * FROM STUDENT WHERE EMAIL = ?',
+      [email],
+      (error, studentRow) => {
+        if (error) {
+          console.error('Error executing SELECT query:', error);
+          return res.status(500).json({ error: 'Internal Server Error' });
+        }
+
+        if (studentRow.length === 0) {
+          return res.status(404).json({ error: 'Student not found.' });
+        }
+
+        if (addToAvailableRooms !== true) {
+          return res.status(400).json({ error: 'Invalid addToAvailableRooms value.' });
+        }
+
+        const { ROOM_ID, STUDENT_ID } = studentRow[0];
+        connection.query(
+          'INSERT INTO AVAILABLE_ROOM (ROOM_ID, STUDENT_ID, ROOM_STATUS, Description) VALUES (?, ?, ?, ?)',
+          [ROOM_ID, STUDENT_ID, 'Y', null],
+          (insertError, result) => {
+
+            if (insertError) {
+              console.error('Error executing INSERT query:', insertError);
+              return res.status(500).json({ error: 'Internal Server Error' });
+            }
+
+            res.status(201).json({ message: 'Your request for room swap has been accepted !!', result });
+          }
+        );
+      }
+    );
+  });
+
+
+  router.get('/checkStudentInAvailableRooms', (req, res) => {
+    const email = req.session.user['email'];
+    console.log(email);
+  
+      connection.query(
+        'SELECT * FROM AVAILABLE_ROOM WHERE STUDENT_ID = (SELECT STUDENT_ID FROM STUDENT WHERE EMAIL = ?)',
+        [email],
+        (error, result) => {
+  
+          if (error) {
+            console.error('Error executing SELECT query:', error);
+            return res.status(500).json({ error: 'Internal Server Error' });
+          }
+  
+          if (result.length > 0) {
+            res.status(200).json({ message: 'yes', result });
+          } else {
+            res.status(404).json({ message: 'not found' });
+          }
+        }
+      );
+    });
+ 
+
+
+
+
 module.exports = router;
